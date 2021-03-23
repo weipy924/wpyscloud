430fa748578be9ee60dc08547bfb2bc1e9858be6
weipengyu <Weipengyu@ibeesaas.com>
2021-03-23 16:40:13 +0800

    update .gitignore

--- cloud-provider-payment8001/src/main/java/com/pengyu/springcloud/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.pengyu.springcloud.service;
-
-import com.pengyu.springcloud.entities.Payment;
-import org.apache.ibatis.annotations.Param;
-
-public interface PaymentService {
-    int create(Payment payment);
-    Payment getPaymentById(@Param("id") Long id);
-}
